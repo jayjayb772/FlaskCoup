1df132d6279baee8432144905c102506efc315fb
jayjayb772 <jayjayb100@msn.com>
2019-07-24 18:19:49 -0500

    updated joined page

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ def mainPage():
 def login():
     return render_template('joinPage.html')
 
-@app.route('/waitingRoom/<UUID>')
-def waitingRoom(UUID):
-    return 'welcome %s' % UUID
+@app.route('/waitingRoom/<player>')
+def waitingRoom(player):
+    return 'welcome %s' % player
     # TODO:
     # Render page that will update when a new player is added
     # Shows how many players and how many of each card
@@ -24,7 +24,7 @@ def joinSubmit():
     player = request.form['Name']
     gameCode = request.form['GameCode']
     uniqueUserID = savePlayerToDB.saveToDB(player, gameCode)
-    return redirect(url_for('waitingRoom', UUID=uniqueUserID))
+    return redirect(url_for('waitingRoom', name=player))
     # TODO:
     # Save things to DB
     # pass game code and returned user ID to page
