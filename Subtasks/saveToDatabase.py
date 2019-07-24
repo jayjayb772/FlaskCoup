2be0488cacc2b50330952c77dc576989d92ba98e
jayjayb772 <jayjayb100@msn.com>
2019-07-23 22:02:31 -0500

    Added pages. you can put in your name and game code.takes you to uniquepage

---
+++ Subtasks/saveToDatabase.py
@@ -0,0 +1,5 @@
+
+
+def saveToDB(player, gameCode):
+    # save to DB and get unique userID for game
+    return "1"
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from Subtasks import saveToDatabase
+
 
 app = Flask(__name__)
 
@@ -7,10 +9,27 @@ app = Flask(__name__)
 def mainPage():
     return render_template('mainPage.html')
 
+@app.route('/join')
+def login():
+    return render_template('joinPage.html')
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/waitingRoom/<UUID>')
+def waitingRoom(UUID):
+    return 'welcome %s' % UUID
+    # TODO:
+    # Render page that will update when a new player is added
+    # Shows how many players and how many of each card
+    # Highlghts user on page
+
+@app.route('/joinSubmit', methods=['POST'])
+def joinSubmit():
+    player = request.form['Name']
+    gameCode = request.form['GameCode']
+    uniqueUserID = saveToDatabase.saveToDB(player, gameCode)
+    return redirect(url_for('waitingRoom', UUID=uniqueUserID))
+    # TODO:
+    # Save things to DB
+    # pass game code and returned user ID to page
 
 
 if __name__ == '__main__':
