10269aab43c25e4635d2f8e8234aed352506cc8e
jayjayb772 <jayjayb100@msn.com>
2019-07-24 02:41:53 -0500

    Update otherJenkins.groovy

--- otherJenkins.groovy
+++ otherJenkins.groovy
@@ -1,13 +1,14 @@
 node{
     docker.image('python:3.7.2')
     stage("checkout code"){
-        checkout scm
+        git 'https://github.com/jayjayb772/FlaskCoup.git'
     }
     stage("get requirements"){
         sh "pip install -r requirements.txt"
     }
     stage("build"){
-        sh "python app.py &"
+        sh "set FLASK_APP=app.py"
+        sh "flask run --host=0.0.0.0"
     }
 
 }
